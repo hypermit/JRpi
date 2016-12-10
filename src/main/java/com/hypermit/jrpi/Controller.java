8414fe65cb26ea49222b6452d5c330d35af719f8
hypermit <hypermit@gmail.com>
2016-12-10 07:20:20 +0330

    add gpio buttons to Gpio Control tab

--- src/main/java/com/hypermit/jrpi/Controller.java
+++ src/main/java/com/hypermit/jrpi/Controller.java
@@ -2,6 +2,7 @@ package com.hypermit.jrpi;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -13,15 +14,98 @@ import javafx.scene.control.TextField;
  */
 public class Controller {
     SshConnect sshConnect = new SshConnect();
-
     @FXML
     private Label lblConnectionStatus;
     @FXML
-    private TextField tfHost, tfUser;
+    private TextField tfHost;
+    @FXML
+    private TextField tfUser;
     @FXML
     private PasswordField pfPassword;
-
-
+    @FXML
+    private Button btnConnect;
+    @FXML
+    private Button btnDisconnect;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Button btn11;
+    @FXML
+    private Button btn13;
+    @FXML
+    private Button btn15;
+    @FXML
+    private Button btn17;
+    @FXML
+    private Button btn19;
+    @FXML
+    private Button btn21;
+    @FXML
+    private Button btn23;
+    @FXML
+    private Button btn25;
+    @FXML
+    private Button btn27;
+    @FXML
+    private Button btn29;
+    @FXML
+    private Button btn31;
+    @FXML
+    private Button btn33;
+    @FXML
+    private Button btn35;
+    @FXML
+    private Button btn37;
+    @FXML
+    private Button btn39;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn10;
+    @FXML
+    private Button btn12;
+    @FXML
+    private Button btn14;
+    @FXML
+    private Button btn16;
+    @FXML
+    private Button btn18;
+    @FXML
+    private Button btn20;
+    @FXML
+    private Button btn22;
+    @FXML
+    private Button btn24;
+    @FXML
+    private Button btn26;
+    @FXML
+    private Button btn28;
+    @FXML
+    private Button btn30;
+    @FXML
+    private Button btn32;
+    @FXML
+    private Button btn34;
+    @FXML
+    private Button btn36;
+    @FXML
+    private Button btn38;
+    @FXML
+    private Button btn40;
 
     public void btnConnectAction(ActionEvent actionEvent) {
         String host = tfHost.getText();
