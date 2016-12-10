2035b030f81521bfbbb70ba06dbe9a13586358a3
hypermit <hypermit@gmail.com>
2016-12-10 07:43:43 +0330

    have you try turning gpio's off and on again? :D

--- src/main/java/com/hypermit/jrpi/Controller.java
+++ src/main/java/com/hypermit/jrpi/Controller.java
@@ -1,8 +1,11 @@
 package com.hypermit.jrpi;
 
+import com.jcraft.jsch.JSchException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.io.IOException;
 /**
  * @author: hypermit
  * Date: 2016
@@ -10,6 +13,13 @@ import javafx.scene.control.*;
  */
 public class Controller {
     SshConnect sshConnect = new SshConnect();
+    private byte gpioPinNumber;
+    private String gpioMode;
+    private String gpioReadCommand;
+    private String gpioStatus;
+    private String gpioSwitchCommand;
+    private byte gpioBitStatus;
+
     @FXML
     private Label lblConnectionStatus;
     @FXML
@@ -103,6 +113,8 @@ public class Controller {
     @FXML
     private Button btn40;
 
+    private Button btn0;
+
     public void btnConnectAction(ActionEvent actionEvent) {
         String host = tfHost.getText();
         String user = tfUser.getText();
@@ -131,7 +143,7 @@ public class Controller {
         }
     }
 
-    public void btnGpioAction(ActionEvent actionEvent) {
+    public void btnGpioAction(ActionEvent actionEvent) throws IOException, JSchException {
         Object btnClicked = actionEvent.getSource();
         if (btnClicked == btn2 || btnClicked == btn4) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -154,7 +166,37 @@ public class Controller {
             alert.setContentText("This gpio pin is GND.");
             alert.showAndWait();
         } else {
-            System.out.println("other gpio clicked");
+            Button btClick = (Button) actionEvent.getSource();
+            String strButtonId = btClick.getId();
+            strButtonId = strButtonId.substring(3);
+            byte pinNumber = Byte.parseByte(strButtonId);
+            switchGpio(pinNumber);
         }
+    }
+
+    private void switchGpio(byte btnNumber) throws IOException, JSchException {
+        Button btnArray[] = {btn0 ,btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10,
+                btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19, btn20,
+                btn21, btn22, btn23, btn24, btn25, btn26, btn27, btn28, btn29, btn30,
+                btn31, btn32, btn33, btn34, btn35, btn36, btn37, btn38, btn39, btn40};
+        gpioPinNumber = gpio2WiringPin(btnNumber);
+        gpioMode = "gpio mode " + gpioPinNumber + " out";
+        sshConnect.send(gpioMode);
+
+        gpioReadCommand = "gpio read " + gpioPinNumber;
+        gpioStatus = sshConnect.send(gpioReadCommand);
+        // get first char from gpio read to remove "\n" and convert it to byte
+        gpioBitStatus = Byte.parseByte(gpioStatus.substring(0,1));
+        gpioBitStatus = (byte) (1 - gpioBitStatus);
+
+        gpioSwitchCommand = "gpio write " + gpioPinNumber + " " + gpioBitStatus;
+        sshConnect.send(gpioSwitchCommand);
+    }
+
+    // convert gpio number to wiring pi number
+    private byte gpio2WiringPin(byte btPinNumber) {
+        byte wiringPiPins[] = {-1, -1, -1 , 8, -1, 9, -1, 7, 15, -1, 16, 0, 1, 2, -1, 3, 4, -1, 5, 12,
+                -1, 13, 6, 14, 10, -1, 11, 30, 31, 21, -1, 22, 26, 23, -1, 24, 27, 25, 28, -1, 29};
+        return wiringPiPins[btPinNumber];
     }
 }
