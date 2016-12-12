1260af1cb9e1e6a7241c28501921f2de5cfa6f88
hypermit <hypermit@gmail.com>
2016-12-13 01:01:09 +0330

    add application icon

--- src/main/java/com/hypermit/jrpi/Main.java
+++ src/main/java/com/hypermit/jrpi/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -16,7 +17,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main_window.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("JRpi");
+        primaryStage.getIcons().add(new Image("/icon.png"));
         primaryStage.setScene(new Scene(root, 750, 400));
         primaryStage.show();
     }
